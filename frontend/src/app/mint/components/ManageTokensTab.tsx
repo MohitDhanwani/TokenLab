59db8074793599df51f4e1f93aa45d319d61b081
Mohit <dhanwaniap@gmail.com>
2025-12-10 19:43:06 +0530

    feat: added manage frontend component

---
+++ frontend/src/app/mint/components/ManageTokensTab.tsx
@@ -0,0 +1,75 @@
+"use client";
+import useWallet from "@/utils/ConnectWallet";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Lock, Settings } from "lucide-react";
+
+interface Token {
+  id: string;
+  tokenName: string;
+  tokenSymbol: string;
+  description: string;
+  currentTotalSupply: string;
+}
+
+export default function ManageTokensTab() {
+  const [connectedWalletTokens, setConnectedWalletTokens] = useState<Token[]>([]);
+  const { walletAddress } = useWallet();
+
+  useEffect(() => {
+    const getAllWalletTokens = async () => {
+      const walletTokens = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/getAllTokens`, {
+        params: { walletAddress },
+      });
+      setConnectedWalletTokens(walletTokens.data.allWalletTokens);
+    };
+
+    getAllWalletTokens();
+  }, [walletAddress]);
+
+  return (
+    <div className="w-full flex justify-center">
+      <div className="w-9/12">
+        <div>
+          <h1 className="font-bold text-3xl font-serif mt-16">YOUR TOKENS</h1>
+        </div>
+
+        <div className="grid grid-cols-3 gap-6 mt-10">
+          {connectedWalletTokens.map((token, index) => (
+            <div className="border-2 border-black px-6 py-6 flex flex-col gap-5 bg-white hover:-translate-y-3 transition-all duration-300 hover:cursor-pointer hover:shadow-[5px_5px_0px_0px_rgba(0,0,0,1)]">
+              <div key={index} className="flex justify-between">
+                <div
+                  className={`h-14 w-16 text-sm p-2 border-2 border-black flex items-center justify-center text-white ${index % 2 !== 0 ? "bg-orange-600" : "bg-black"}`}
+                >
+                  {token.tokenSymbol}
+                </div>
+
+                <div className="px-4 font-mono border-2 border-black h-8 flex items-center bg-gray-100 text-sm">ACTIVE</div>
+              </div>
+
+              <div className="border-b-2 border-gray-400 opacity-70 pb-4">
+                <h1 className="font-extrabold font-mono text-xl">{token.tokenName}</h1>
+                <h3 className="font-mono pt-1">{token.tokenSymbol}</h3>
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <div className="flex justify-between">
+                  <h3 className="text-sm">SUPPLY</h3>
+                  <h2 className="font-bold text-mono">{token.currentTotalSupply}</h2>
+                </div>
+
+                <div className="flex justify-between">
+                  <h3 className="text-sm">AUTH</h3>
+                  <div className="font-bold text-mono flex gap-2">
+                    <Settings size={20} />
+                    <Lock size={20} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/mint/page.tsx
+++ frontend/src/app/mint/page.tsx
@@ -1,31 +1,38 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Wallet from "./components/Wallet";
 import CreateToken from "./components/CreateToken";
 import AllTokens from "./components/AllTokens";
+import ManageTokensTab from "./components/ManageTokensTab";
 
 const MintPage = () => {
+  const [activeTab, setActiveTab] = useState("CREATE NEW");
+
   return (
     <>
       <div className="w-full flex justify-center pt-16">
         <div className="flex items-center justify-between w-full max-w-7xl border-b-4 border-black pb-2">
           <h1 className="text-5xl font-extrabold font-serif">DASHBOARD</h1>
           <div className="flex gap-4 text-sm font-bold font-mono">
-            <div className="text-white bg-black p-4 hover:cursor-pointer">CREATE NEW</div>
-            <div className="text-black bg-[#F4F4F0] p-4 border-2 border-black hover:cursor-pointer">MANAGE EXTENSIONS</div>
+            <div className="text-white bg-black p-4 hover:cursor-pointer" onClick={() => setActiveTab("CREATE")}>
+              CREATE NEW
+            </div>
+            <div className="text-black bg-[#F4F4F0] p-4 border-2 border-black hover:cursor-pointer" onClick={() => setActiveTab("MANAGE")}>
+              MANAGE EXTENSIONS
+            </div>
           </div>
         </div>
       </div>
 
-      <div className="w-full flex justify-center">
+      {activeTab === "CREATE" ? (<div className="w-full flex justify-center">
         <div className="flex items-start justify-between w-full max-w-7xl gap-10">
           <div className="flex flex-col w-full max-w-3xl">
             <Wallet />
             <AllTokens />
           </div>
-            <CreateToken />
+          <CreateToken />
         </div>
-      </div>
+      </div>) : (<ManageTokensTab/>)}
     </>
   );
 };
