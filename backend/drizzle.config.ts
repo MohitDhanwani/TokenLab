76af769da33d1decf0229183b0eff435c262d91a
Mohit <dhanwaniap@gmail.com>
2025-12-12 11:59:56 +0530

    added build script to backend package.json

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from "drizzle-kit";
 import { DATABASE_URL } from "./src/config/index"
 import "dotenv/config.js"
 
-console.log(DATABASE_URL);
-
 export default defineConfig({
   schema: "./src/schema.ts",
   out: "./drizzle",
